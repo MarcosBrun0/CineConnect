3931bb4ed56a5fa5b9f8c3855eab7ada99919954
marcosbrun0 <Mbrunocampos20@gmail.com>
2025-08-22 19:35:31 -0300

    commit inicial

---
+++ src/test/java/com/cinema/CineConnect/CineConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinema.CineConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
