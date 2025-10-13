c21ed19cca254125ca3b2386992bdd5a7e100fdc
rodrigollb28 <rodrigobrand28@gmail.com>
2025-10-13 19:28:03 -0300

    started creating the login page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,24 +1,16 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';import './App.css'
-import { Button } from "@mantine/core";
+import {Form} from "@mantine/form";
+import Login from "./pages/public/Login"
 
 
-function Example(){
-    return(
-        <div>
-        <h1>Example!</h1>
-        <Button>Example</Button>
-        </div>
-    )
-
-}
 function App() {
   const [count, setCount] = useState(0)
 
   return (
       <Router>
           <Routes>
-              <Route path="/" element=<Example/> />
+              <Route path="/" element={<Login/>} />
           </Routes>
               </Router>
 
