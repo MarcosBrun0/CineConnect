4a91488223d5bb634c90c2e6c4360de42a7dcfe4
rodrigollb28 <rodrigobrand28@gmail.com>
2025-10-14 18:34:31 -0300

    Login form layout with email validation

--- front-end/src/pages/public/Login.jsx
+++ front-end/src/pages/public/Login.jsx
@@ -1,19 +1,91 @@
-import { Anchor, Group, PasswordInput, Text } from '@mantine/core';
+import { Anchor, PasswordInput, Text, TextInput, Flex, Button, Card} from '@mantine/core';
+import { useForm } from '@mantine/form';
+
 
 function Login() {
+
+    const form = useForm(
+        {
+        mode: 'uncontrolled',
+        initialValues: { email: '', password: ''},
+        validate:{
+            email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email')
+            },
+
+        });
+
     return (
-        <>
-            <Group justify="space-between" mb={5}>
-                <Text component="label" htmlFor="your-password" size="sm" fw={500}>
-                    Your password
+        <div>
+            <Card
+            shadow="lg"
+            withBorder
+            radius="md"
+            >
+            <Flex
+                justify="space-between"
+                direction='column'
+                p="sm"
+            >
+
+                <form onSubmit={form.onSubmit((values) => console.log(values))}>
+                    <Text component="label" htmlFor="your-email" size="sm" pb="xs" fw={500}>
+                    Your Email
                 </Text>
 
-                <Anchor href="#" onClick={(event) => event.preventDefault()} pt={2} fw={500} fz="xs">
+            <TextInput
+                placeholder="youremail@example.com"
+                key={form.key('email')}
+                {...form.getInputProps('email')}
+                id="your-email"
+                pb="xs">
+
+            </TextInput>
+
+                <Text component="label" htmlFor="your-password" size="sm" pb="xs" fw={500}>
+                    Your password
+
+                </Text>
+
+                    <PasswordInput placeholder="Your password"
+                                   id="your-password"
+                                   key={form.key('password')}
+                                   {...form.getInputProps('password')}                                   pb="xs"
+                    />
+
+
+                <Button
+                type="submit"
+                radius="md"
+                mt="md"
+                >Login</Button>
+
+                </form>
+                <Card.Section
+                    withBorder
+                    inheritPadding
+                    mt="sm"
+                    p="sm">
+
+                    <Anchor
+                        href="#" onClick={(event) => event.preventDefault()} pt={2} fw={500} fz="xs">
+                        Register
+                    </Anchor>
+
+                <Anchor href="#"
+                        ml="lg"
+                        onClick={(event) => event.preventDefault()} pt={2} fw={500} fz="xs"
+                >
+
                     Forgot your password?
                 </Anchor>
-            </Group>
-            <PasswordInput placeholder="Your password" id="your-password" />
-        </>
+                </Card.Section>
+
+
+            </Flex>
+            </Card>
+
+        </div>
+
     );
 }
 
